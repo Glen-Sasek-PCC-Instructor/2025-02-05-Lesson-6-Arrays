7eb0727a6cac4490804b899a6cc3986388b42590
Glen Sasek <glen.sasek@pcc.edu>
2025-02-12 19:46:45 +0000

    Add youtube dataset example

---
+++ youtube-dataset/youtube.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+df = pd.read_csv('yt-1000.csv')
+
+print(df)
+
+print(df.head())  # By default, shows the first 5 rows
+
+print(df.describe())
+
+# Remove commas from numbers for numeric sorting
+df['Video views'] = df['Video views'].str.replace(',', '').astype(float)
+df['Likes'] = df['Likes'].str.replace(',', '').astype(float)
+df['Dislikes'] = df['Dislikes'].str.replace(',', '').astype(float)
+
+print("")
+print("")
+print("TOP TEN LIKES")
+print(df.nlargest(10, 'Likes'))
+
+print("")
+print("")
+print("TOP TEN DISLIKES")
+print(df.nlargest(10, 'Dislikes'))
+
+print("")
+print("")
+print("TOP TEN VIEWS")
+print(df.nlargest(10, 'Video views'))
